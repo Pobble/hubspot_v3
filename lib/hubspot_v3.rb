7d14d682ac9098463e9b056c192aec9b989f4036
Tomas Valent <equivalent@eq8.eu>
2021-10-13 14:53:58 +0200

    find email

--- lib/hubspot_v3.rb
+++ lib/hubspot_v3.rb
@@ -1,8 +1,46 @@
 # frozen_string_literal: true
 
+require 'httparty'
+require 'json'
 require_relative "hubspot_v3/version"
 
 module HubspotV3
-  class Error < StandardError; end
-  # Your code goes here...
+  class RequestFailedError < StandardError; end
+
+  API_URL='https://api.hubapi.com'
+  SEARCH_CONTACTS='/crm/v3/objects/contacts/search'
+
+  def self.contacts_search_by_email(email)
+    body = {
+      "filterGroups":[
+        {
+          "filters": [
+            {
+              "propertyName": "email",
+              "operator": "EQ",
+              "value": email
+            }
+          ]
+        }
+      ]
+    }
+    post(SEARCH_CONTACTS, body)
+  end
+
+  def self.url(path)
+    "#{API_URL}#{path}?hapikey=#{config.apikey}"
+  end
+
+  def self.post(path, body)
+    res = HTTParty.post(url(path), {
+      body: body.to_json,
+      headers: {'Content-Type' => 'application/json'}
+    })
+    case res.code
+    when 200
+      res.parsed_response['results']
+    else
+      raise(RequestFailedError, res.message)
+    end
+  end
 end
---
+++ lib/hubspot_v3/config.rb
@@ -0,0 +1,13 @@
+module HubspotV3
+  class Config
+    attr_writer :apikey
+
+    def apikey
+      @apikey || raise('Hubspot API key is not set. Set it with HubspotV3.config.apikey="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"')
+    end
+  end
+
+  def self.config
+    @config ||= Config.new
+  end
+end
