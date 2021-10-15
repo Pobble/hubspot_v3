d5dd27a2cdc26119f563459a07b36b1266510cb9
Tomas Valent <equivalent@eq8.eu>
2021-10-15 16:12:22 +0200

    mappedemail

--- lib/hubspot_v3/config.rb
+++ lib/hubspot_v3/config.rb
@@ -5,10 +5,6 @@ module HubspotV3
     def apikey
       @apikey || raise('Hubspot API key is not set. Set it with HubspotV3.config.apikey="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"')
     end
-
-    def contract
-      @contract || HubspotV3
-    end
   end
 
   def self.config
